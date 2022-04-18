248682c8e767c0115e709187af3ab79177e72a1f
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-17 23:57:52 -0400

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace assignment_log_and_reg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
